26d88dad081152d386a869d74b04b2666385329b
Vasu Bansal <2001vasubansal2001@gmail.com>
2024-02-20 19:01:03 +0530

    Added header and footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark fixed-bottom">
+        <h5 className="text-start mx-3" style={{ color: "white" }}>
+          Campus Hall Booking System
+        </h5>
+        <h7 className="text-end mx-3" style={{ color: "white" }}>
+          Developed by Team 1
+        </h7>
+      </nav>
+    </>
+  );
+};
+
+export default Footer;
