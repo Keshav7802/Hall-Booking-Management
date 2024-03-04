17aa8e5cb31984098fea6dcc977cfebb22be6bef
Vasu Bansal <2001vasubansal2001@gmail.com>
2024-03-05 00:22:07 +0530

    Added the side bar and created routes

--- client/src/components/SideNavigation.js
+++ client/src/components/SideNavigation.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { Link } from "react-router-dom";
 
-const SideNavigation = () => {
+const SideNavigation = ({}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Sidebar style={{ height: "inherit" }}>
+      <Menu
+        menuItemStyles={{
+          button: {
+            // the active class will be added automatically by react router
+            // so we can use it to style the active menu item
+            [`&.active`]: {
+              backgroundColor: "#13395e",
+              color: "#b6c8d9",
+            },
+          },
+        }}
+      >
+        <MenuItem component={<Link to="/documentation" />}>
+          {" "}
+          Documentation
+        </MenuItem>
+        <MenuItem component={<Link to="/calendar" />}> Calendar</MenuItem>
+        <MenuItem component={<Link to="/e-commerce" />}> E-commerce</MenuItem>
+      </Menu>
+    </Sidebar>
+  );
+};
 
-export default SideNavigation
+export default SideNavigation;
