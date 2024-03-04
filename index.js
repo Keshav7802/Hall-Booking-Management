575532f3473e841f07e80a39888522e3c00a073c
rohitmadan07 <madan.rohit502@gmail.com>
2024-03-04 16:20:00 +0530

    Connect to MongoDB

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello To Hall Booking API');
+})
+
+const PORT = process.env.PORT || 5000; 
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=> app.listen(PORT,()=>console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error)=> console.log(`${error} did not connect`));
