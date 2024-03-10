a797e09056ca5d0307fb953dc81090d95efeba85
Suyashvarshney8 <108462601+Suyashvarshney8@users.noreply.github.com>
2024-03-10 16:58:30 +0530

    Implemented CSS styling in Hall Availability file.

--- client/src/components/HallAvailability.js
+++ client/src/components/HallAvailability.js
@@ -1,41 +1,33 @@
-// import React from 'react';
-// import Hall_Navigation from './HallAvailability_sidebar';
-// import CalendarMain from './Calendar'
-// import '../css/LoginPage.css';
-// import Calendar from 'react-calendar';
-// const columnStyle = {
-//   width: '100%',
-//   padding: '0rem 0rem 0rem 1rem',
-//   boxSizing: 'border-box',
-//   backgroundColor: '#f0f0f0',
-// };
-
-// const HomePage = () =>  {
-//   return (
-//     <div className="flex flex-col md:flex-row">
-//       <Hall_Navigation
-//       />
-      
-//       <div style={{ ...columnStyle }}> <CalendarMain /></div>
-//     </div>
-//   );
-// }
-
-// export default HomePage;
 import React from 'react';
 import Hall_Navigation from './HallAvailability_sidebar';
 import CalendarMain from './Calendar';
 import '../css/LoginPage.css';
+import Calendar from 'react-calendar';
 
+const containerStyle = {
+  display: 'flex',
+  marginTop: '-1em',
+};
 
+const sidebarStyle = {
+  position: 'fixed',
+  backgroundColor: '#f0f0f0',
+};
+
+const contentStyle = {
+  marginLeft: '250px',
+  width: '90%',// Same as the width of the sidebar
+  boxSizing: 'border-box',
+  padding: '0rem 0rem 0rem 1rem',
+};
 
 const HomePage = () => {
   return (
-    <div style={{display: 'flex'}}>
-      <div style={{position: 'fixed'}}>
+    <div style={containerStyle}>
+      <div style={sidebarStyle}>
         <Hall_Navigation />
       </div>
-      <div style={{marginLeft: '250px', width: '100%'}}>
+      <div style={contentStyle}>
         <CalendarMain />
       </div>
     </div>
