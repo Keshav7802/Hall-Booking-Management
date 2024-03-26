fcbc884930b3b23327efc57dbb785acc56527859
Omkar More <moreomkar4842@gmail.com>
2024-03-26 14:33:05 +0530

    Revised changes in membership routes

--- server/routes/membership.js
+++ server/routes/membership.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createMembership, deleteMembership } from "./../controllers/membership.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post('/createMembership', createMembership);
-router.post('/deleteMembership', deleteMembership);
+router.post('/createMembership',auth, createMembership);
+router.delete('/deleteMembership',auth, deleteMembership);
 
 export default router;
