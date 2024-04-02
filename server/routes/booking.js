e64d57f10ce69592a0c5b02a66b00689137eb616
rohitmadan07 <madan.rohit502@gmail.com>
2024-04-02 14:22:25 +0530

    Adding auth middleware to protect required routes

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createBooking, getAllBookings, updateBooking } from "../controllers/booking";
+import { createBooking, getAllBookings, updateBooking } from "../controllers/booking.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/createBooking",createBooking); 
+router.post("/createBooking",auth,createBooking); 
 router.get("/getAllBookings",getAllBookings); 
-router.patch("/updateBooking/:id",updateBooking); 
+router.patch("/updateBooking/:id",auth,updateBooking); 
 
 export default router;
--- server/routes/event.js
+++ server/routes/event.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createEvent, deleteEvent, getAllEvents } from './../controllers/event.js';
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get('/getAllEvents', getAllEvents);
-router.post('/createEvent', createEvent);
-router.delete('/deleteEvent/:eventID', deleteEvent);
+router.post('/createEvent', auth, createEvent);
+router.delete('/deleteEvent/:eventID', auth, deleteEvent);
 
 export default router;
