86851900d1d13b38e766d65370beeb82c557adff
Vasu Bansal <2001vasubansal2001@gmail.com>
2024-04-07 15:38:35 +0530

    "Update event creation in server/controllers/event.js, remove unnecessary fields; update user signin and signup in server/controllers/users.js to include user ID; update event model in server/models/event.js to remove unnecessary fields"

--- client/src/components/UserContext.js
+++ client/src/components/UserContext.js
@@ -27,14 +27,13 @@ const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userName, setUserName] = useState('');
-
+  const [userId,setUserId] = useState('');
   const logout = () => {
     setUserName(''); 
-    
   };
 
   return (
-    <UserContext.Provider value={{ userName, setUserName, logout }}>
+    <UserContext.Provider value={{ userName, setUserName, logout ,userId,setUserId}}>
       {children}
     </UserContext.Provider>
   );
--- server/models/event.js
+++ server/models/event.js
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 
 const eventSchema = mongoose.Schema({
-  eventID: Number,
+  // eventID: Number,
   eventName: String,
   eventType: String,
-  clubID: Number,
+  // clubID: Number,
+  clubName: String,
   eventPurpose: String,
   startDateTime: Date,
   endDateTime: Date,
   eventDuration: Number,
-  ticketBooking: Boolean,
-  ticketID: Number,
+  // ticketBooking: Boolean,
+  // ticketID: Number,
 });
 
 const EventModel = mongoose.model('EventModel', eventSchema);
