acd04bce2a1a2465aa19a1f80c4945345f078097
Keshav7802 <78keshav02@gmail.com>
2024-04-17 05:20:00 +0530

    Sending events to calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -2,35 +2,36 @@ import React from 'react';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
-import events from './Events'
-
-// const customLocalizer = momentLocalizer(moment, {
-//   formats: {
-//     yearHeaderFormat: 'YYYY',
-//   },
-//   messages: {
-//     year: 'Year',
-//   },
-// });
-
-// const CustomDay = ({ date }) => {
-//   const columns = Array.from({ length: 10}, (_, index) => index + 1);
-
-//   return (
-//     <div style={{ display: 'flex', flexDirection: 'row' }}>
-//       {columns.map((column, index) => (
-//         <div key={index}>
-//           <div style={{ fontWeight: 'bold' }}>{column}</div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
+import { useEffect, useState } from "react";
 
 const localizer = momentLocalizer(moment);
 
 const CalendarMain = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("http://localhost:3001/booking/getAllBookings", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${userData.token}`,
+        },
+      });
+      const hallData = await data.json();
+
+      // Transform booking data into events list
+      const events = hallData.map((booking) => ({
+        title: booking.eventName,
+        start: new Date(booking.startDateTime),
+        end: new Date(booking.endDateTime),
+      }));
+
+      // Set the events list in state
+      setEvents(events);
+    };
+    fetchData();
+  }, []);
 
 
   return (
