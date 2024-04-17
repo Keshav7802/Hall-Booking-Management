5a8af18484442c8a4a390f0946fcaf12e5de2261
Vasu Bansal <2001vasubansal2001@gmail.com>
2024-04-17 16:16:06 +0530

    Minor changes

--- client/src/components/UserContext.js
+++ client/src/components/UserContext.js
@@ -1,26 +1,3 @@
-// // UserContext.js
-// import React, { createContext, useState } from 'react';
-
-// const UserContext = createContext();
-
-// export const UserProvider = ({ children }) => {
-//   const [userName, setUserName] = useState('');
-
-//   const logout = () => {
-//     setUserName(''); 
-//   };
-
-//   return (
-//     <UserContext.Provider value={{ userName, setUserName }}>
-//       {children}
-//     </UserContext.Provider>
-//   );
-// };
-
-// export default UserContext;
-
-
-
 import React, { createContext, useState } from "react";
 
 const UserContext = createContext();
